70a1e16e6617ae619d6cbc5666fd96aedffdfba4
SansDeltarune1 <115196377+SansDeltarune1@users.noreply.github.com>
2025-06-23 06:53:33 -0500

    b

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ chef: https://i.pinimg.com/474x/3c/35/36/3c3536050f0e6349f36b4617a924e8de.jpg
 base walt: https://upload.wikimedia.org/wikipedia/en/thumb/0/03/Walter_White_S5B.png/250px-Walter_White_S5B.png
  explosion: https://media.tenor.com/nANqORN7qhQAAAAM/explosion-explode.gif
  this is art yo : https://64.media.tumblr.com/7e12e1a961c94e8c94dd5d11180a4c99/25e3ec6958f00365-d8/s540x810/6b7fbd6e45e42319b08785c6efdcdabf8697b2a5.gifv
-
+ high five gif : https://media.tenor.com/cS5jqwPsXAwAAAAM/high-five-walter-white.gif
+ let him cook : https://media.tenor.com/0nCouv9MFkUAAAAM/meme.gif
+ jail : https://hardinthecity.wordpress.com/wp-content/uploads/2013/09/breaking-bad-to_hajiilee-walter-white-bars-jail.jpg
 */
 let optionOne = document.querySelector(".option-one");
 let optionTwo = document.querySelector(".option-two");
@@ -33,6 +35,14 @@ optionThree.addEventListener("click",function(){
     optionTwo.innerHTML = " For a boss";
     optionThree.style.display = "none";
   }
+  if(sProgress === 3 && rv === false){
+      mainText.textContent = " You need to lift a heavy barrel of ingredients, what do you do?"
+      optionOne.style.display = "block";
+    optionOne.innerHTML = "Use a forklift."
+    optionTwo.style.display = "block";
+    optionTwo.innerHTML = "Use your hands.";
+    optionThree.style.display = "none";
+    }
     else if (sProgress ===4 && rv === true ){
         mainText.textContent = "Great now all you have to do is pick someone to sell it to"
         mainImg.src = "https://i.redd.it/9afzt4xnidq41.jpg";
@@ -46,13 +56,15 @@ optionThree.addEventListener("click",function(){
   sProgress++;
 });
 optionOne.addEventListener("click", function(){
-    if(sProgress === 2){
+    if(sProgress === 2 ){
+      rv = true;
      mainText.textContent = "You start cooking, what do you want to use while cooking?"
+     mainImg.src = "https://media.tenor.com/0nCouv9MFkUAAAAM/meme.gif";
      optionOne.innerHTML = "Sodium Bicarbonate";
      optionTwo.innerHTML = "Nitrogen Triiodide";
 
     }
-    if(sProgress ===3){
+    if(sProgress ===3 && rv ===true){
      mainText.textContent = "Good job, you used baking soda and finished your meal";
      mainImg.src = "https://64.media.tumblr.com/7e12e1a961c94e8c94dd5d11180a4c99/25e3ec6958f00365-d8/s540x810/6b7fbd6e45e42319b08785c6efdcdabf8697b2a5.gifv"
      optionThree.style.display = "block";
@@ -60,18 +72,80 @@ optionOne.addEventListener("click", function(){
         optionOne.style.display = "none";
         optionTwo.style.display = "none";
     }
+    if(sProgress === 5 && rv === true){
+       mainText.textContent = "It was a cop and you got sent to jail";
+       mainImg.src = "https://hardinthecity.wordpress.com/wp-content/uploads/2013/09/breaking-bad-to_hajiilee-walter-white-bars-jail.jpg";
+       restart = true;
+       optionThree.style.display = "block";
+      optionThree.innerHTML = "Restart";
+      optionOne.style.display = "none";
+      optionTwo.style.display = "none";
+    }
+    if(sProgress === 4 && rv === false){
+      mainText.textContent = "You were not forklift certified."
+      mainImg.src = "https://studyingbreakingbadcourse.wordpress.com/wp-content/uploads/2015/05/ep-405-31m11s-tyrus-runs-the-fork-lift.png"
+      restart = true;
+       optionThree.style.display = "block";
+      optionThree.innerHTML = "Restart";
+      optionOne.style.display = "none";
+      optionTwo.style.display = "none";
+    }
+    if(sProgress === 5 && rv === false){
+      restart = true
+      mainText.textContent = " You are a millionare now!"
+      mainImg.src = "https://i.ytimg.com/vi/_pcIvMwDqks/maxresdefault.jpg"
+      optionThree.style.display = "block";
+      optionThree.innerHTML = "Restart";
+      optionOne.style.display = "none";
+      optionTwo.style.display = "none";
+        }
  sProgress++;
 });
 
 optionTwo.addEventListener("click", function(){
-    if(sProgress === 3){
+    if(sProgress === 2){
+      rv = false;
+      mainImg.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqAViwmsEUKIaT0vjsvqDVFMwq40tTnvD7QQ&s";
+      mainText.textContent = "You start to work with a rich resturant owner, you agree to show him your recipe for a million dollars."
+      optionThree.style.display = "block";
+        optionThree.innerHTML = "continue";
+        optionOne.style.display = "none";
+        optionTwo.style.display = "none";
+    }
+    
+
+    if(sProgress === 3 && rv ===true){
         mainImg.src = "https://media.tenor.com/nANqORN7qhQAAAAM/explosion-explode.gif";
-        mainText.textContent = "wrong one, you blew up :(";
+        mainText.textContent = "Wrong one, you blew up :(";
         restart = true;
         optionThree.style.display = "block";
         optionThree.innerHTML = "Restart";
         optionOne.style.display = "none";
         optionTwo.style.display = "none";
+    }
+    if(sProgress === 5 && rv === true){
+      mainText.textContent = "Congradulations you made a bunch of money and are rich!!!"
+      mainImg.src = "https://media.tenor.com/cS5jqwPsXAwAAAAM/high-five-walter-white.gif";
+       restart = true;
+       optionThree.style.display = "block";
+      optionThree.innerHTML = "Restart";
+      optionOne.style.display = "none";
+      optionTwo.style.display = "none";
+    }
+    if(sProgress===4 && rv === false){
+      mainText.textContent = "You finished the recipe but officers broke into the building, what should you do."
+      mainImg.src = "https://i.ytimg.com/vi/zVjCYOKPzjg/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDLhABG6QQYIBzurJoWgHrDHrf_8Q"
+      optionOne.innerHTML = " Grab ur money and run"
+      optionTwo.innerHTML = "Hide"
+    }
+    if(sProgress ===5 && rv === false){
+      mainText.textContent = "They found you and took you to jail"
+      mainImg.src = " https://hardinthecity.wordpress.com/wp-content/uploads/2013/09/breaking-bad-to_hajiilee-walter-white-bars-jail.jpg"
+       restart = true;
+       optionThree.style.display = "block";
+      optionThree.innerHTML = "Restart";
+      optionOne.style.display = "none";
+      optionTwo.style.display = "none";
     }
     sProgress++;
 })
